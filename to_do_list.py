96fb5a5774db124f8f39b70d9c1ac1638fd673cd
b-smith94 <byrondsmith9@gmail.com>
2024-08-08 18:13:19 -0400

    restarted progress on the delete_task function, but completed the complete_tasks function

--- to_do_list.py
+++ to_do_list.py
@@ -1,18 +1,18 @@
 menu = ["Add a task", "view tasks", "Mark a task as complete", "Delete a task", "Quit"]
 
-
-to_do_list = []    
+incomplete_tasks = []
+finished_tasks = []   
 
 def add_task(task_list):
     while True:
         new_task = input("Please provide the task you wish to add: ")
-        to_do_list.append(new_task)
+        incomplete_tasks.append(new_task)
         while True:
             more_new_task = input("Would you like to add another task? (yes/no): ")
             if more_new_task.lower() == 'yes':
                 break
             elif more_new_task.lower() == 'no':
-                print(f"To do list: {to_do_list}") 
+                print(f"\nTo do list: {incomplete_tasks}") 
                 break
             else:
                 print("invalid input, please try again.")
@@ -21,36 +21,41 @@ def add_task(task_list):
             continue
         else:
             break
-def view_tasks(task_list):
-    print(f"\nTo-Do List: \n{to_do_list}\n")
+
+def view_tasks(task_list_1, task_list_2):
+    print(f"\n--Incomplete tasks: {incomplete_tasks}")
+    print(f"--Finished tasks: {finished_tasks}")
+    
 
 def complete_tasks(task_list):
-    pass
-def delete_tasks(task_list):
     while True:
-        try:
-            remove_task = input("Please provide the task you wish to remove: ")
-            to_do_list.remove(remove_task)
-        except ValueError:
-            print("Item is not on the list, please try again.")
-            break
-        else:
-            while True:
-                remove_more_task = input("Would you like to remove another task? (yes/no): ")
-                if remove_more_task.lower() == 'yes':
-                    break
-                elif remove_more_task.lower() == 'no':
-                    print(f"To do list: {to_do_list}") 
-                    break
-                else:
-                    print("invalid input, please try again.")
-                    continue
-            if remove_more_task.lower() == 'yes':
-                continue
+        completed_task = input("Enter the task you wish to mark as complete: ")
+        if completed_task in incomplete_tasks:
+            if completed_task not in finished_tasks:
+                finished_tasks.append(completed_task)
+                incomplete_tasks.remove(completed_task)
             else:
+                print("This task is already marked as complete.")
+        else:
+            print("Task not found.")
+        while True:
+            more_complete = input("Would you like to mark another task? (yes/no): ")
+            if more_complete.lower() == 'yes':
                 break
+            elif more_complete.lower() == 'no':
+                print(f"\nFinished Tasks: {finished_tasks}\nIncomplete Tasks: {complete_tasks}")
+                break
+            else:
+                print("Invalid input, please try again.")
+                continue
+        if more_complete.lower() == 'yes':
+            continue
+        else:
+            break
+def delete_tasks(task_list_1, task_list_2):
+    pass
 
-def quit(task_list):
+def quit():
     print("\nThank you for using the To-Do List app! Have a wonderful day!\n")
     
 
@@ -67,15 +72,15 @@ while True:
         print("Error: Invalid input, please enter an integer (example: 5).")
     else:
         if choice == 1:
-            add_task(to_do_list)
+            add_task(incomplete_tasks)
         elif choice == 2:
-            view_tasks(to_do_list)
+            view_tasks(incomplete_tasks, finished_tasks)
         elif choice == 3:
-            complete_tasks(to_do_list)
+            complete_tasks(incomplete_tasks)
         elif choice == 4:
-            delete_tasks(to_do_list)
+            delete_tasks(incomplete_tasks, finished_tasks)
         else:
-            quit(to_do_list)
+            quit()
             break
         
         
